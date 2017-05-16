75928e3c0c5319f018676881570a31cce04ee761
codemasterkarol <codemasterkarol@gmail.com>
2017-05-16 11:22:11 -0500

    Mobile Friendly

--- includes/header.php
+++ includes/header.php
@@ -6,12 +6,15 @@
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="">
+    <meta name="description" content="From planning to deployment, and development to support. Whether it's your personal site, or a project for a client. Let this site guide you through building a web presence that is both informative and easy to use.">
+    <meta property="og:image" content="/assets/img/ogimage.png"/>
+    <meta name="keywords" content="Web Design Best Practices, Web Design Best Practices 2017, Web Design Trends, Web Design, Web Development, Web Development Best Practices">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/g/foundation@6.2.1(foundation.min.css)">
     <link href="https://fonts.googleapis.com/css?family=Glass+Antiqua|Montserrat|Slabo+27px" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/font-awesome.css">
     <link rel="stylesheet" href="/assets/css/style.css">
+
 </head>
 
 <header>
