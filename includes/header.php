9fb11007d2f14b2e8f03c29dda1c20e36b1b23d9
codemasterkarol <codemasterkarol@gmail.com>
2017-05-16 17:37:06 -0500

    Fixing a color

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
-<?php $sitetitle = 'WDBP.'; ?>
+<?php $sitetitle = 'WDBP.';?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?= $sitetitle; ?></title>
+    <title><?= $sitetitle;?></title>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
